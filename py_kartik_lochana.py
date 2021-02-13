fbcb1f1a75e0b5e2670895bf140c80f9d4dcd841
lochana-kartik-tkcs <lochanasrinivas2001@gmail.com>
2021-02-13 20:25:10 +0530

    I added fun2

--- py_kartik_lochana.py
+++ py_kartik_lochana.py
@@ -13,3 +13,13 @@ def fun1(arr,l,r,x)://~kartik
 			return fun1(arr, mid+1,r,x)
 	else:
 		return -1
+
+def fun2(a, b)://~Lochana
+	print("Original value of a is", a)
+	print("Original value of b is", b)
+
+	temp = a
+	a=b
+	b=temp
+	print("Changed value of a is",a)
+	print("Changed value of b is",b)
